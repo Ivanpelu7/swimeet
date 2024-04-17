284832afa0e963259a8ced978e5544719bbab4c1
Ivanpelu7 <ivanpelufo98@gmail.com>
2024-04-17 11:41:09 +0200

    splashscreen

--- app/src/main/java/com/example/swimeet/ui/MainFragment.kt
+++ app/src/main/java/com/example/swimeet/ui/MainFragment.kt
@@ -6,8 +6,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.swimeet.R
-import com.example.swimeet.databinding.FragmentChatBinding
 import com.example.swimeet.databinding.FragmentMainBinding
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
